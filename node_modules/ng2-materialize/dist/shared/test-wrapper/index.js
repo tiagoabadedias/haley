0944da69e1bc79ca2362e2eee511e30581703315
Tiago Dias <tiago@infoworld.com.br>
2018-02-22 10:54:54 -0300

    Iniciando

--- node_modules/ng2-materialize/dist/shared/test-wrapper/index.js
+++
@@ -1,3 +0,0 @@
-export * from './test-wrapper.builder';
-export * from './test-wrapper.component';
-export * from './test-wrapper.module';
--- node_modules/ngx-mask/build/index.js
+++
@@ -1,2 +0,0 @@
-export * from './ngx-mask.module';
-export * from './mask.directive';
